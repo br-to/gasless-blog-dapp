fe70f796dac91acf84d6809403d38b2d64020f4f
br-to <barabara0224@gmail.com>
2025-07-21 02:28:02 +0900

    refactor: 不要な空行を削除し、PostManagerコントラクトに投稿数を取得する関数を追加

--- contracts/PostManager.sol
+++ contracts/PostManager.sol
@@ -9,16 +9,16 @@ pragma solidity ^0.8.28;
 contract PostManager {
     /// @notice Auto-increment primary key for posts
     uint256 public postCount;
-    
+
     /// @notice Mapping from post ID to IPFS/Bundlr CID
     mapping(uint256 => string) public posts;
-    
+
     /// @notice Mapping from post ID to author address
     mapping(uint256 => address) public authors;
-    
+
     /// @notice Event emitted when a new post is created
     event PostCreated(uint256 indexed id, address indexed author, string cid);
-    
+
     /**
      * @dev Creates a new post with the given CID
      * @param cid The IPFS/Bundlr CID of the post content
@@ -28,15 +28,15 @@ contract PostManager {
         // Increment post count to get new post ID
         postCount++;
         postId = postCount;
-        
+
         // Store the post data
         posts[postId] = cid;
         authors[postId] = msg.sender;
-        
+
         // Emit event
         emit PostCreated(postId, msg.sender, cid);
     }
-    
+
     /**
      * @dev Returns the post CID for a given post ID
      * @param postId The post ID to query
@@ -45,7 +45,7 @@ contract PostManager {
     function getPost(uint256 postId) external view returns (string memory cid) {
         return posts[postId];
     }
-    
+
     /**
      * @dev Returns the author address for a given post ID
      * @param postId The post ID to query
@@ -54,7 +54,7 @@ contract PostManager {
     function getAuthor(uint256 postId) external view returns (address author) {
         return authors[postId];
     }
-    
+
     /**
      * @dev Returns the post data for a given post ID
      * @param postId The post ID to query
@@ -63,5 +63,13 @@ contract PostManager {
      */
     function getPostData(uint256 postId) external view returns (string memory cid, address author) {
         return (posts[postId], authors[postId]);
+    }
+
+    /**
+     * @dev Returns the total number of posts created
+     * @return count The total number of posts
+     */
+    function getTotalPosts() external view returns (uint256 count) {
+        return postCount;
     }
 }
